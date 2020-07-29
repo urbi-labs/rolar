53a51127628c09e4e1abb0951a71d9e16d3ab770
Matias Salimbene <msalimbe@ar.ibm.com>
2020-07-29 17:09:08 -0300

    added user model

--- server/models/user.js
+++ server/models/user.js
@@ -1,73 +1,61 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
-const jwt = require("jsonwebtoken");
-const config = require("config");
+
+// const jwt = require("jsonwebtoken");
+// const config = require("config");
 const Joi = require("joi");
 Joi.objectId = require("joi-objectid")(Joi);
 
 const userSchema = new Schema({
-  name: {
+  name: { type: String, uppercase: true, required: true, trim: true },
+  mail: {
     type: String,
-    uppercase: true,
+    lowercase: true,
     required: true,
     trim: true,
+    unique: true,
   },
   password: {
     type: String,
-    minlength: 8,
+    minlength: 5,
     maxlenght: 16,
     required: true,
     trim: true,
   },
-  mail: {
-    type: String,
-    maxlenght: 255,
-    lowercase: true,
-    unique: true,
-    trim: true,
-  },
-  _origin: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Bank",
-  },
-  isAdmin: { type: Boolean, default: false },
-  isActive: { type: Boolean, default: true },
+  role: { type: String, enum: ["operator", "supervisor"], default: "operator" },
   timestamp: { type: Date, default: Date.now },
 });
-
-userSchema.methods.generateAuthToken = function () {
-  const token = jwt.sign(
-    {
-      _id: this._id,
-      name: this.name,
-      mail: this.mail,
-      _origin: this._origin,
-      isAdmin: this.isAdmin,
-      isActive: this.isActive,
-    },
-    config.get("jwtPrivateKey")
-  );
-  return token;
-};
-
-userSchema.statics.lookup = function (_origin) {
-  return this.findById(_origin).populate("_origin", "", "Origin");
-};
 
 const User = mongoose.model("User", userSchema);
 
 function validateUserSchema(user) {
   const schema = {
-    name: Joi.string().required(),
-    password: Joi.string().required(),
-    mail: Joi.string().required(),
-    _origin: Joi.objectId(),
-    isActive: Joi.boolean(),
-    isAdmin: Joi.boolean(),
+    name: Joi.string().require(),
+    mail: Joi.string().mail().require(),
+    password: Joi.string().min(5).max(16).required(),
   };
 
   return Joi.validate(user, schema);
 }
 
-module.exports.User = User;
 module.exports.validate = validateUserSchema;
+module.exports.User = User;
+
+// userSchema.methods.generateAuthToken = function () {
+//   const token = jwt.sign(
+//     {
+//       _id: this._id,
+//       name: this.name,
+//       mail: this.mail,
+//       _origin: this._origin,
+//       isAdmin: this.isAdmin,
+//       isActive: this.isActive,
+//     },
+//     config.get("jwtPrivateKey")
+//   );
+//   return token;
+// };
+
+// userSchema.statics.lookup = function (_origin) {
+//   return this.findById(_origin).populate("_origin", "", "Origin");
+// };
