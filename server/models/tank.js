570a94887abf1443945b6e1aa739387a28b7eb01
Gaston Lifschitz <glifschitz@itba.edu.ar>
2020-08-05 16:54:05 -0300

    Added Tank model

---
+++ server/models/tank.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+const { ObjectId } = Schema.Types;
+
+const Joi = require("joi");
+Joi.objectId = require("joi-objectid")(Joi);
+
+const tankSchema = new Schema({
+  description: { type: String, required: true, trim: true },
+  active: { type: Boolean, default: false },
+  capacity: { type: Number, required: true },
+  timestamp: { type: Date, default: Date.now },
+});
+
+const Tank = mongoose.model("Tank", tankSchema);
+
+function validateTankSchema(tank) {
+  const schema = {
+    description: Joi.string().required(),
+    active: Joi.boolean(),
+    capacity: Joi.number().required(),
+  };
+
+  return Joi.validate(tank, schema);
+}
+
+module.exports.validate = validateTankSchema;
+module.exports.Tank = Tank;
