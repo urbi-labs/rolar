33eecf57d36621de138f24eb64d3dc37949e6b74
Gaston Lifschitz <glifschitz@itba.edu.ar>
2020-08-05 16:51:24 -0300

    Added Storage model

---
+++ server/models/storage.js
@@ -0,0 +1,64 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+const { ObjectId } = Schema.Types;
+
+const Joi = require("joi");
+Joi.objectId = require("joi-objectid")(Joi);
+
+const storageSchema = new Schema({
+  _batch: {
+    type: ObjectId,
+    ref: "Batch",
+    required: true,
+  },
+  _user: {
+    type: ObjectId,
+    ref: "User",
+    required: true,
+  },
+  _tank: {
+    type: ObjectId,
+    ref: "Tank",
+    required: true,
+  },
+  _supervisor: {
+    type: ObjectId,
+    ref: "User",
+    required: true,
+  },
+  cone: { type: String, required: true, trim: true },
+  initialMeasure: { type: Number, required: true },
+  finalMeasure: { type: Number, required: true },
+  totalCm: { type: Number, required: true },
+  totalLitres: { type: Number, required: true },
+  oilWeight: { type: Number, required: true },
+  performance: { type: Number, required: true },
+  validated: { type: Boolean, default: false },
+  validationDate: { type: Date },
+  timestamp: { type: Date, default: Date.now },
+});
+
+const Storage = mongoose.model("Storage", storageSchema);
+
+function validateStorageSchema(storage) {
+  const schema = {
+    _batch: Joi.objectId().required(),
+    _user: Joi.objectId().required(),
+    _supervisor: Joi.objectId().required(),
+    _tank: Joi.objectId().required(),
+    validated: Joi.boolean(),
+    validationDate: Joi.date(),
+    cone: Joi.string().required(),
+    initialMeasure: Joi.number().required(),
+    finalMeasure: Joi.number().required(),
+    totalCm: Joi.number().required(),
+    totalLitres: Joi.number().required(),
+    oilWeight: Joi.number().required(),
+    performance: Joi.number().required(),
+  };
+
+  return Joi.validate(storage, schema);
+}
+
+module.exports.validate = validateStorageSchema;
+module.exports.Storage = Storage;
