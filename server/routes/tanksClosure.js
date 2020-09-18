e21bd5d65dd5719d75fff7bcd603841fa0377625
Matias Salimbene <msalimbe@ar.ibm.com>
2020-09-18 16:58:17 -0300

    completed api implementation

--- server/routes/tanksClosure.js
+++ server/routes/tanksClosure.js
@@ -1,5 +1,7 @@
 const auth = require("../middleware/auth");
 const { TankClosure, validate } = require("../models/tankClosure");
+const { Tank } = require("../models/tank");
+const { Storage } = require("../models/storage");
 const express = require("express");
 const router = express.Router();
 
@@ -10,10 +12,30 @@ router.post("/", auth, async (req, res) => {
   if (error) return res.status(400).send(error.details[0].message);
 
   const tankClosure = new TankClosure({ ...body });
-  tankClosure.batchArray = [];
-  await tankClosure.save();
 
-  return res.status(200).send(tankClosure);
+  const { _tank } = body;
+
+  // revisar filtro por fecha: tomar ultimo closure y filtrar storages en adelante
+  // const lastClosureDate = await TankClosure.find({ _tank })
+  //   .sort({
+  //     timestamp: -1,
+  //   })
+  //   .exec();
+
+  const batchArray = await Storage.find({ _tank }).select(
+    "totalLitres timestamp"
+  );
+
+  tankClosure.batchArray = batchArray;
+
+  try {
+    await tankClosure.save();
+    await Tank.findByIdAndUpdate({ _id: _tank }, { active: false });
+
+    return res.status(200).send(tankClosure);
+  } catch (error) {
+    return res.status(500).send("Contacte al administrador.");
+  }
 });
 
 module.exports = router;
