8c77bdd6fdda8577525fd4207fdf914d032ebba1
Matias Salimbene <msalimbe@ar.ibm.com>
2020-09-24 16:26:26 -0300

    working on foodtrust api

--- server/services/xmlMsgs.js
+++ server/services/xmlMsgs.js
@@ -11,8 +11,6 @@ const uuidv1 = require("uuid/v1");
 const xml2js = require("xml2js");
 const parseString = util.promisify(xml2js.parseString);
 
-const { header, sender, receiver } = require("../XMLMessages/XML_Tags.json");
-const { docId, instance, date, id } = require("../XMLMessages/XML_Tags.json");
 const { exp, sellBy, best, lot } = require("../XMLMessages/XML_Tags.json");
 const {
   farmId: fid,
@@ -20,72 +18,10 @@ const {
   farm: f,
 } = require("../XMLMessages/XML_Tags.json");
 
-const { FTPI, FTLI } = require("../XMLMessages/GS1_Patterns.json");
-const { LFTPI, FTLPN } = require("../XMLMessages/GS1_Patterns.json");
+const { LFTPI, FTLPN, FTLI } = require("../XMLMessages/GS1_Patterns.json");
 const { UUID, DISP } = require("../XMLMessages/GS1_Patterns.json");
 
 const PREFIX = config.get("FT_COMPANY_PREFIX");
-
-const mdItem = async (values) => {
-  const {
-    item_root: r,
-    epcis_tradeItemData: tid,
-  } = require("../XMLMessages/XML_Tags.json");
-  const { item_desc, item_sku } = values;
-
-  const item_ref = Math.random().toString(36).substring(2, 7);
-
-  const xmlFilePath = path.join(cwd, "XMLMessages", "MDItem.xml");
-  const xmlFile = await fs.readFile(xmlFilePath);
-  const xml = await parseString(xmlFile);
-
-  xml[r][header][0][sender][0][id][0]._ = ""; //Mandatory: GLN (global location number) for the party who sent this item information
-  xml[r][header][0][receiver][0][id][0]._ = ""; //Mandatory, but can be left empty
-  xml[r][header][0][docId][0][instance][0] = ""; //Mandatory: Document identifier which uniquely identifies this message
-  xml[r][header][0][docId][0][date][0] = new Date().toISOString(); //Mandatory: Time when this message was created
-  xml[r][tid][0].tradeItemDescription[0]._ = item_desc; //Mandatory: English language description of item
-  xml[r][tid][0].gtin = `${FTPI}:${PREFIX}.${item_ref}`; //urn:ibm:ift:product:class:2244002.00013
-  xml[r][tid][0].sku = item_sku;
-  // xml[r][tid][0].dataSource[0].gln = `${FTLI}:${PREFIX}.snnl`; //urn:ibm:ift:location:loc:2244002.snnl
-  // xml[r][tid][0].dataRecipient[0].gln = `${FTLI}:${PREFIX}.snnl`; //urn:ibm:ift:location:loc:2244002.snnl
-
-  const builder = new xml2js.Builder();
-  const outputXml = builder.buildObject(xml);
-
-  log(xml[r]);
-
-  return outputXml;
-};
-
-const mdFacility = async (values) => {
-  const { party_root: r } = require("../XMLMessages/XML_Tags.json");
-  const { facility_name, facility_role, facility_owner } = values;
-  const xmlFilePath = path.join(cwd, "XMLMessages", "MDFacility.xml");
-  const xmlFile = await fs.readFile(xmlFilePath);
-  const xml = await parseString(xmlFile);
-
-  const loc_ref = Math.random().toString(36).substring(2, 7);
-
-  xml[r][header][0][sender][0][id][0]._ = ""; //Mandatory: GLN (global location number) for the party who sent this item information
-  xml[r][header][0][receiver][0][id][0]._ = ""; //Mandatory, but can be left empty
-  xml[r][header][0][docId][0][instance][0] = ""; //Mandatory: Document identifier which uniquely identifies this message
-  xml[r][header][0][docId][0][date][0] = new Date().toISOString();
-  xml[r].party[0].isPartyActive[0] = true;
-  xml[r].party[0].registeringParty[0] = `${FTLI}:${PREFIX}.growth`;
-  xml[r].party[0].gln[0] = `${FTLI}:${PREFIX}.${loc_ref}`;
-  xml[r].party[0].partyAddress[0].city[0] = "";
-  xml[r].party[0].partyAddress[0].countryCode[0] = "Argentina";
-  xml[r].party[0].partyAddress[0].name[0] = facility_name;
-  xml[r].party[0].partyAddress[0].postalCode[0] = "";
-  xml[r].party[0].partyRole[0].partyName[0] = facility_owner;
-  xml[r].party[0].partyRole[0].partyRoleCode[0] = facility_role;
-
-  log(JSON.stringify(xml[r]));
-  const builder = new xml2js.Builder();
-  const outputXml = builder.buildObject(xml);
-
-  return outputXml;
-};
 
 const epcisCommission = async (values) => {
   const {
@@ -266,8 +202,6 @@ const epcisAggregation = async (values) => {
   return outputXml;
 };
 
-exports.mdItem = mdItem;
-exports.mdFacility = mdFacility;
 exports.epcisCommission = epcisCommission;
 exports.epcisTransformation = epcisTransformation;
 exports.epcisObservation = epcisObservation;
