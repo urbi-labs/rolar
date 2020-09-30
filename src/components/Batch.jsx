36c88d8ef07cace789e05432124e37f4fb0467f4
Matias Salimbene <msalimbe@ar.ibm.com>
2020-09-30 11:41:37 -0300

    working on screens

--- src/components/Batch.jsx
+++ src/components/Batch.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import Batch1 from "../batch/Batch1";
-import Batch2 from "../batch/Batch2";
-import Batch3 from "../batch/Batch3";
+import Batch1 from "./batch/Batch1";
+import Batch2 from "./batch/Batch2";
+import Batch3 from "./batch/Batch3";
 
 const Batch = ({ data, step, submit }) => {
   console.log("rendering Batch...");
