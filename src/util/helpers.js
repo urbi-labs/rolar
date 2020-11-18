4c17c52e3400cfe58b36121711e42019a4a0463e
Matias Salimbene <msalimbe@ar.ibm.com>
2020-11-18 16:35:29 -0300

    adjusted storage calcs in front end helper function

--- src/util/helpers.js
+++ src/util/helpers.js
@@ -6,12 +6,12 @@ export function calcs(
   netWeight
 ) {
   const { doc } = tank;
-  const { radius } = doc;
-  console.log({ netWeight });
+  const { radius, cone: coneCapacity } = doc;
 
+  const conevalue = cone ? 0.5 * coneCapacity : 0;
   const tot_cm = finalMeasure - initialMeasure;
   const tot_lt =
-    cone + Math.PI * ((Math.pow(radius / 1000, 2) * tot_cm) / 1000) * 1000;
+    conevalue + Math.PI * ((Math.pow(radius / 1000, 2) * tot_cm) / 1000) * 1000;
   const oilWeight = tot_lt * 0.92;
 
   const perf = oilWeight / netWeight;
