a2f9c6ffff150958f4a91ae92a4694e0d0755b97
Matias Salimbene <msalimbe@ar.ibm.com>
2020-11-25 10:26:25 -0300

    clean up

--- server/startup/prod.js
+++ server/startup/prod.js
@@ -33,10 +33,6 @@ module.exports = function (app) {
       index: false,
       setHeaders: (res, path) => {
         res.setHeader("Cache-Control", "public, immutable, max-age=31536000");
-        res.setHeader(
-          "Content-Security-Policy",
-          "default-src * self blob: data: gap:; style-src * self 'unsafe-inline' blob: data: gap:; script-src * 'self' 'unsafe-eval' 'unsafe-inline' blob: data: gap:; object-src * 'self' blob: data: gap:; img-src * self 'unsafe-inline' blob: data: gap:; connect-src self * 'unsafe-inline' blob: data: gap:; frame-src * self blob: data: gap:;"
-        );
       },
     })
   );
