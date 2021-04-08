b5f2d9f56484a47766ce835ba022860a016a7dc9
Matias Salimbene <msalimbe@ar.ibm.com>
2021-04-08 11:43:44 -0300

    clean up

--- server/routes/xls.js
+++ server/routes/xls.js
@@ -10,7 +10,6 @@ const express = require("express");
 const router = express.Router();
 
 const path = require("path");
-const { type } = require("os");
 const cwd = process.cwd();
 
 const sheets = {
